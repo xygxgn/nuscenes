6eb44eca1d1ab4d1e1ea56baf3aadf6489659b29
wu lifeng <wulifeng@autowise.ai>
2025-05-08 18:46:10 +0800

    first commit

---
+++ osm/nusc_map_handler.py
@@ -0,0 +1,335 @@
+import numpy as np
+from nuscenes.map_expansion.map_api import NuScenesMap, NuScenesMapExplorer
+from nuscenes.eval.common.utils import quaternion_yaw, Quaternion
+from shapely import affinity, ops
+from shapely.geometry import LineString, box, MultiPolygon, MultiLineString
+import geopandas as gpd
+from matplotlib.collections import LineCollection
+import matplotlib.pyplot as plt
+from const import CLASS2LABEL
+import os
+from typing import Tuple, List
+
+class NuscMapHandler(object):
+    """
+    nuscenes map handler
+    """
+    def __init__(self,
+                 dataroot: str,
+                 patch_size: Tuple,
+                 canvas_size: Tuple,
+                 line_classes: List[str] = ['road_divider', 'lane_divider'],
+                 ped_crossing_classes: List[str] = ['ped_crossing'],
+                 contour_classes: List = ['road_segment', 'lane'],
+                 sample_dist: int = 1,
+                 num_samples: int = 250,
+                 padding: bool = False,
+                 normalize: bool = False,
+                 fixed_num: int = -1):
+        '''
+        Args:
+            fixed_num = -1 : no fixed num
+        '''
+        super().__init__()
+        self.data_root = dataroot
+        # maps in nuscenes
+        self.nusc_maps = [
+            'boston-seaport',     
+            'singapore-hollandvillage', 
+            'singapore-onenorth', 
+            'singapore-queenstown'
+        ]
+        self.line_classes = line_classes                 # ['road_divider', 'lane_divider']
+        self.ped_crossing_classes = ped_crossing_classes # ['ped_crossing']
+        self.polygon_classes = contour_classes           # ['road_segment', 'lane'] 
+        self.nusc_maps = {map_name: NuScenesMap(dataroot=self.data_root, map_name=map_name) for map_name in self.nusc_maps}
+        self.map_explorer = {map_name: NuScenesMapExplorer(self.nusc_maps[map_name]) for map_name in self.nusc_maps}
+        
+        # self.nusc_maps = {}
+        # self.map_explorer = {}
+        # for map_name in self.nusc_maps:
+        #     self.nusc_maps[map_name] = [NuScenesMap(dataroot=self.data_root, map_name=map_name) for map_name in self.nusc_maps]
+        #     self.map_explorer[map_name] = [NuScenesMapExplorer(self.nusc_maps[map_name]) for map_name in self.nusc_maps]
+
+        self.patch_size = patch_size
+        self.canvas_size = canvas_size
+        self.sample_dist = sample_dist
+        self.num_samples = num_samples
+        self.padding = padding
+        self.normalize = normalize
+        self.fixed_num = fixed_num
+
+
+    def get_osm_geom(self, patch_box, patch_angle, location):
+        osm_map = self.sd_maps[location]
+        patch_x = patch_box[0]
+        patch_y = patch_box[1]
+        patch = NuScenesMapExplorer.get_patch_coord(patch_box, patch_angle)
+        line_list = []
+        for geom_line in osm_map.geoms:
+            if geom_line.is_empty:
+                continue
+            new_line = geom_line.intersection(patch)
+            if not new_line.is_empty:
+                new_line = affinity.rotate(new_line, -patch_angle, origin=(patch_x, patch_y), use_radians=False)
+                new_line = affinity.affine_transform(new_line,
+                                                     [1.0, 0.0, 0.0, 1.0, -patch_x, -patch_y])
+                line_list.append(new_line)
+        return line_list
+
+    def gen_vectorized_samples(self, location, ego2global_translation, ego2global_rotation):
+        map_pose = ego2global_translation[:2]
+        rotation = Quaternion(ego2global_rotation)
+        patch_box = (map_pose[0], map_pose[1], self.patch_size[0], self.patch_size[1])
+        patch_angle = quaternion_yaw(rotation) / np.pi * 180
+
+        # a. 获取线类型(road_divider, lane_divider)
+        line_geom = self.get_map_geom(patch_box, patch_angle, self.line_classes, location) 
+        line_vector_dict = self.line_geoms_to_vectors(line_geom)
+        # b. 获取人行道
+        ped_geom = self.get_map_geom(patch_box, patch_angle, self.ped_crossing_classes, location)
+        # ped_vector_list = self.ped_geoms_to_vectors(ped_geom)
+        ped_vector_list = self.line_geoms_to_vectors(ped_geom)['ped_crossing']
+        # c. 获取道路线(roda_segment, lane)
+        polygon_geom = self.get_map_geom(patch_box, patch_angle, self.polygon_classes, location)
+        poly_bound_list, union_roads = self.poly_geoms_to_vectors(polygon_geom)
+
+        vectors = []
+        for line_type, vects in line_vector_dict.items():
+            for line, length in vects:
+                vectors.append((line.astype(float), length, CLASS2LABEL.get(line_type, -1))) 
+        # CLASS2LABEL = {'road_divider': 0,'lane_divider': 0,'ped_crossing': 1,'contours': 2,'others': -1}
+        for ped_line, length in ped_vector_list:
+            vectors.append((ped_line.astype(float), length, CLASS2LABEL.get('ped_crossing', -1)))
+
+        for contour, length in poly_bound_list:
+         
+            vectors.append((contour.astype(float), length, CLASS2LABEL.get('contours', -1)))
+
+        filtered_vectors = []
+        for pts, pts_num, type in vectors:
+            if type != -1:
+                filtered_vectors.append({
+                    'pts': pts,
+                    'pts_num': pts_num,
+                    'type': type
+                })
+
+        osm_geom = self.get_osm_geom(patch_box, patch_angle, location)
+        osm_vector_list = []
+
+        def is_intersection(line):
+            if line.intersects(union_roads):
+                return True
+            else:
+                return False
+            
+        for line in osm_geom:
+            if not line.is_empty:
+                if line.geom_type == 'MultiLineString':
+                    for single_line in line.geoms:
+                        osm_vector_list.append(self.sample_fixed_pts_from_line(single_line, padding=False, fixed_num=50))
+                elif line.geom_type == 'LineString':
+                    osm_vector_list.append(self.sample_fixed_pts_from_line(line, padding=False, fixed_num=50))
+                else:
+                    raise NotImplementedError
+
+        return filtered_vectors, polygon_geom, osm_vector_list 
+
+    def get_map_geom(self, patch_box, patch_angle, layer_names, location):
+        map_geom = []
+        for layer_name in layer_names:
+            if layer_name in self.line_classes:
+                geoms = self.map_explorer[location]._get_layer_line(patch_box, patch_angle, layer_name)
+                map_geom.append((layer_name, geoms))
+            elif layer_name in self.polygon_classes:
+                geoms = self.map_explorer[location]._get_layer_polygon(patch_box, patch_angle, layer_name)
+                map_geom.append((layer_name, geoms))
+            elif layer_name in self.ped_crossing_classes:
+                geoms = self.get_ped_crossing_line(patch_box, patch_angle, location)
+                map_geom.append((layer_name, geoms))
+        return map_geom
+
+    def _one_type_line_geom_to_vectors(self, line_geom):
+        line_vectors = []
+        for line in line_geom:
+            if not line.is_empty:
+                if line.geom_type == 'MultiLineString':
+                    for single_line in line.geoms:
+                        line_vectors.append(self.sample_pts_from_line(single_line))
+                elif line.geom_type == 'LineString':
+                    line_vectors.append(self.sample_pts_from_line(line))
+                else:
+                    raise NotImplementedError
+        return line_vectors
+
+    def poly_geoms_to_vectors(self, polygon_geom):
+        roads = polygon_geom[0][1] 
+        lanes = polygon_geom[1][1] 
+        union_roads = ops.unary_union(roads) 
+        union_lanes = ops.unary_union(lanes) 
+        union_segments = ops.unary_union([union_roads, union_lanes])
+        max_x = self.patch_size[1] / 2
+        max_y = self.patch_size[0] / 2
+        local_patch = box(-max_x + 0.2, -max_y + 0.2, max_x - 0.2, max_y - 0.2)
+        exteriors = []
+        interiors = []
+        if union_segments.geom_type != 'MultiPolygon':
+            union_segments = MultiPolygon([union_segments])
+        for poly in union_segments.geoms:
+            exteriors.append(poly.exterior) 
+            for inter in poly.interiors:
+                interiors.append(inter)
+
+        results = []
+        for ext in exteriors:
+            if ext.is_ccw: 
+                ext.coords = list(ext.coords)[::-1]
+            lines = ext.intersection(local_patch) 
+            if isinstance(lines, MultiLineString):
+                lines = ops.linemerge(lines)
+            results.append(lines)
+
+        for inter in interiors:
+            if not inter.is_ccw:
+                inter.coords = list(inter.coords)[::-1]
+            lines = inter.intersection(local_patch)
+            if isinstance(lines, MultiLineString):
+                lines = ops.linemerge(lines)
+            results.append(lines)
+
+        return self._one_type_line_geom_to_vectors(results), union_roads
+
+    def line_geoms_to_vectors(self, line_geom):
+        line_vectors_dict = dict()
+        for line_type, a_type_of_lines in line_geom:
+            one_type_vectors = self._one_type_line_geom_to_vectors(a_type_of_lines)
+            line_vectors_dict[line_type] = one_type_vectors
+
+        return line_vectors_dict
+
+    def ped_geoms_to_vectors(self, ped_geom):
+        ped_geom = ped_geom[0][1]
+        union_ped = ops.unary_union(ped_geom)
+        if union_ped.geom_type != 'MultiPolygon':
+            union_ped = MultiPolygon([union_ped])
+
+        max_x = self.patch_size[1] / 2
+        max_y = self.patch_size[0] / 2
+        local_patch = box(-max_x + 0.2, -max_y + 0.2, max_x - 0.2, max_y - 0.2)
+        results = []
+        for ped_poly in union_ped:
+            ext = ped_poly.exterior
+            if not ext.is_ccw:
+                ext.coords = list(ext.coords)[::-1]
+            lines = ext.intersection(local_patch)
+            results.append(lines)
+
+        return self._one_type_line_geom_to_vectors(results)
+
+    def get_ped_crossing_line(self, patch_box, patch_angle, location):
+        def add_line(poly_xy, idx, patch, patch_angle, patch_x, patch_y, line_list):
+            points = [(p0, p1) for p0, p1 in zip(poly_xy[0, idx:idx + 2], poly_xy[1, idx:idx + 2])]
+            line = LineString(points)
+            line = line.intersection(patch)
+            if not line.is_empty:
+                line = affinity.rotate(line, -patch_angle, origin=(patch_x, patch_y), use_radians=False)
+                line = affinity.affine_transform(line, [1.0, 0.0, 0.0, 1.0, -patch_x, -patch_y])
+                line_list.append(line)
+
+        patch_x = patch_box[0]
+        patch_y = patch_box[1]
+
+        patch = NuScenesMapExplorer.get_patch_coord(patch_box, patch_angle)
+        line_list = []
+        records = getattr(self.nusc_maps[location], 'ped_crossing')
+        for record in records:
+            polygon = self.map_explorer[location].extract_polygon(record['polygon_token'])
+            poly_xy = np.array(polygon.exterior.xy)
+            dist = np.square(poly_xy[:, 1:] - poly_xy[:, :-1]).sum(0)
+            x1, x2 = np.argsort(dist)[-2:]
+
+            add_line(poly_xy, x1, patch, patch_angle, patch_x, patch_y, line_list)
+            add_line(poly_xy, x2, patch, patch_angle, patch_x, patch_y, line_list)
+
+        return line_list
+
+    def sample_pts_from_line(self, line):
+        if self.fixed_num < 0:
+            distances = np.arange(0, line.length, self.sample_dist)
+            sampled_points = np.array([list(line.interpolate(distance).coords) for distance in distances]).reshape(-1, 2)
+        else:
+            # fixed number of points, so distance is line.length / self.fixed_num
+            distances = np.linspace(0, line.length, self.fixed_num)
+            sampled_points = np.array([list(line.interpolate(distance).coords) for distance in distances]).reshape(-1, 2)
+
+        if self.normalize:
+            sampled_points = sampled_points / np.array([self.patch_size[1], self.patch_size[0]])
+
+        num_valid = len(sampled_points)
+
+        if not self.padding or self.fixed_num > 0:
+            # fixed num sample can return now!
+            return sampled_points, num_valid
+
+        # fixed distance sampling need padding!
+        num_valid = len(sampled_points)
+
+        if self.fixed_num < 0:
+            if num_valid < self.num_samples:
+                padding = np.zeros((self.num_samples - len(sampled_points), 2))
+                sampled_points = np.concatenate([sampled_points, padding], axis=0)
+            else:
+                sampled_points = sampled_points[:self.num_samples, :]
+                num_valid = self.num_samples
+
+            if self.normalize:
+                sampled_points = sampled_points / np.array([self.patch_size[1], self.patch_size[0]])
+                num_valid = len(sampled_points)
+
+        return sampled_points, num_valid
+
+    def sample_fixed_pts_from_line(self, line, padding=False, fixed_num=100):
+        '''padding=True,间隔1m均匀采样,根据fixed_num不足进行补0,超过直接舍弃
+        padding=False,根据fixed_num变步长进行采样,以满足长度要求'''
+        if padding:
+            distances = np.arange(0, line.length, self.sample_dist)
+            sampled_points = np.array([list(line.interpolate(distance).coords) for distance in distances]).reshape(-1, 2)
+        else:
+            distances = np.linspace(0, line.length, fixed_num)
+            sampled_points = np.array([list(line.interpolate(distance).coords) for distance in distances]).reshape(-1, 2)
+
+        num_valid = len(sampled_points)
+
+        if num_valid < fixed_num:
+            padding = np.zeros((fixed_num - len(sampled_points), 2))
+            sampled_points = np.concatenate([sampled_points, padding], axis=0)
+        elif num_valid > fixed_num:
+            sampled_points = sampled_points[:fixed_num, :]
+            num_valid = fixed_num
+        if self.normalize:
+            sampled_points = sampled_points / np.array([self.patch_size[1], self.patch_size[0]])
+            num_valid = len(sampled_points)
+
+        num_valid = len(sampled_points)
+        return sampled_points, num_valid
+
+    def write(self, output_floder: str = './output/nuscenes'):
+        for map_name in self.nusc_maps:
+            fig, ax = self.nusc_maps[map_name].render_layers(self.nusc_maps[map_name].non_geometric_layers, figsize=1)
+            ax.axis('equal')
+            output_path = os.path.join(output_floder, '{}.png'.format(map_name))
+            plt.savefig(output_path)
+            plt.close()
+
+if __name__ == '__main__':
+    xbound = [-60.0, 60.0, 0.3] #120m*60m, bev_size:400*200
+    ybound = [-30.0, 30.0, 0.3]
+    patch_h = 20
+    patch_w = 20
+    patch_h = ybound[1] - ybound[0]
+    patch_w = xbound[1] - xbound[0]
+    canvas_h = int(patch_h / ybound[2])
+    canvas_w = int(patch_w / xbound[2])
+    handler = NuscMapHandler(dataroot='./data/sets/nuscenes', patch_size=(patch_h, patch_w), canvas_size=(canvas_h, canvas_w))
+    handler.write(output_floder='./output/nuscenes')
---
+++ osm/osm_map_handler.py
@@ -0,0 +1,68 @@
+from typing import List
+import os
+from nuscenes.map_expansion.map_api import NuScenesMap, NuScenesMapExplorer
+from shapely import affinity
+from shapely.geometry import MultiLineString
+import geopandas as gpd
+from matplotlib.collections import LineCollection
+import matplotlib.pyplot as plt
+from const import MAP_ORIGIN
+from geo import TopocentricConverter
+
+class OsmMapHandler(object):
+    """
+    vectorized osm map
+    """
+    def __init__(self, 
+                 sd_map_path: str = './data/sets/osm'):
+        '''
+        Args:
+            sd_map_path: path to OpenStreetMap(OSM) data
+        '''
+        super().__init__()
+        self.sd_map_path = sd_map_path
+        self.nusc_maps = {}
+
+    def download(self):
+        pass
+
+    def convert(self, maps: List = list(MAP_ORIGIN), options: List[str] = ['living_street', 'road']):
+        '''
+        Args:
+            maps: list of sd_maps to be converted
+        '''
+        converted_sd_maps = dict()
+        for map_name in maps:
+            lat, lon, alt = MAP_ORIGIN[map_name]
+            sd_map = gpd.read_file(os.path.join(self.sd_map_path, '{}.shp'.format(map_name)))
+            converter = TopocentricConverter(lat, lon, alt)
+            sd_map = sd_map[sd_map['type'].isin(options)]
+            sd_map_topo_list = [
+                [converter.to_topocentric(lon, lat, 0.)[:2] for lat, lon in coords]
+                for coords in sd_map.geometry.apply(lambda x: list(x.coords))
+            ]
+            converted_sd_maps[map_name] = MultiLineString(sd_map_topo_list)
+        return converted_sd_maps
+
+    def write(self, converted_sd_maps: dict, output_floder: str = './output/osm'):
+        for map_name in converted_sd_maps:
+            fig, ax = plt.subplots(figsize=(10, 10))
+            lines = LineCollection([list(line.coords) for line in converted_sd_maps[map_name]],
+                                    colors='red',
+                                    linewidths=1)
+            ax.add_collection(lines)
+            ax.axis('equal')
+            ax.grid(True)
+            output_path = os.path.join(output_floder, '{}.png'.format(map_name))
+            plt.savefig(output_path)
+            plt.close()
+
+if __name__ == '__main__':
+    handler = OsmMapHandler(sd_map_path='./data/sets/osm')
+    options = [
+        'trunk', 'primary', 'secondary', 'tertiary', 'unclassified', 'residential', # road
+        'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link'# road link
+        'living_street',  'road',  # Special road  'service'
+    ]
+    converted_sd_maps = handler.convert(maps=list(MAP_ORIGIN), options=options)
+    handler.write(converted_sd_maps)
